a77fb8d9baa7af9d9a1312b30fb77a36cea77adc
Leonardo Avila <lmavilaa@utn.edu.ec>
2023-07-14 00:04:39 -0500

    commit "conexion"

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=92.249.44.207;dbname=u911849556_inven", "u911849556_inven", "L@LKiE11N|s9");
+		$link = new PDO("mysql:host=92.249.44.207;dbname=u911849556_inventario", "u911849556_inventario", "L@LKiE11N|s9");
 		
 		$link->exec("set names utf8");
 
