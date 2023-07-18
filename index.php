855e6c579064b3ff37cb80a51026b7bb05a8d5a6
LeoAvila1911 <51346121+LeoAvila1911@users.noreply.github.com>
2023-07-17 23:07:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Mostrar errores
 
 ini_set('display_errors', 1);
 ini_set("log_errors", 1);
-ini_set("error_log",  "C:/xampp/htdocs/pos/php_error_log");
+ini_set("error_log",  "php_error_log");
 
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/usuarios.controlador.php";
