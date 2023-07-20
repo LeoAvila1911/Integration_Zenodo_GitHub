5d862be185293bda77ba76decf33b2fd9c01f220
Leonardo Avila <lmavilaa@utn.edu.ec>
2023-07-20 00:29:00 -0500

    prueba 0.1.1

--- controladores/github.controlador.php
+++ controladores/github.controlador.php
@@ -3,6 +3,6 @@ require '../modelos/github.modelo.php';
 $github=new Github();
 
 $github->realizarPush();
-$github->crearRelease();
+/* $github->crearRelease(); */
 header('Location: ../ventas?github=success');
 ?>
